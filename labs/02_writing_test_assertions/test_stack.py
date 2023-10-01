16e76fd4a106383c117306e95ba02f3398b4f160
riswanmsm <msmr1986@gmail.com>
2023-10-01 09:15:53 +0530

    test cases added in lab 2

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,28 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(2)
+        self.assertEqual(self.stack.peek(), 2)
+        self.stack.push(4)
+        self.assertEqual(self.stack.peek(), 4)
+        
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(2)
+        self.stack.push(4)
+        self.assertEqual(self.stack.pop(), 4)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertTrue(self.stack.is_empty())
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push(2)
+        self.stack.push(4)
+        self.assertEqual(self.stack.peek(), 4)
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push(2)
+        self.assertFalse(self.stack.is_empty())
